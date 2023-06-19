039c8cf73e597e39f37f82e3e3100bcae577d85b
john <johnmaluki58@gmail.com>
2023-06-19 14:13:14 +0300

    Complete assignment on array find method

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+const superbowlWin = (records) => {
+  const win = records.find((record) => record.result === "W");
+
+  if (win) {
+    return win.year;
+  } else {
+    return undefined;
+  }
+};
